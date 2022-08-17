ca69b21d292a2f15e8b771834062959de972aa9d
F8711399 Rodrigo de Lima Carvalho <rodrigo.lima@bb.com.br>
2022-08-17 13:41:07 -0300

    get top 20 horro movies.
     Co-authored-by: Rogerio <rogerghira@gmail.com>

--- src/main/java/com/filmes/App.java
+++ src/main/java/com/filmes/App.java
@@ -1,13 +1,24 @@
 package com.filmes;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import models.Filme;
 import models.FilmeBuilder;
+import models.Genero;
 
 
 public class App {
@@ -15,23 +26,60 @@ public class App {
     private static final String REGEX = ",(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
 
     public static void main(String[] args) throws IOException {
-        CarregarFilmes("resources/movies-csv/movies3.csv", false);
+        List<Filme> filmes = CarregarFilmes("resources/movies-csv/movies3.csv", false);
+        filmes.addAll(CarregarFilmes("resources/movies-csv/movies2.csv", false));
+        filmes.addAll(CarregarFilmes("resources/movies-csv/movies1.csv", true));
+
+        getTop20Horror(filmes);
+
     }
 
-    private static void CarregarFilmes(String path, Boolean skipHeader) {
+    private static void getTop20Horror(List<Filme> filmes) {
+        List<Filme> top20 = filmes.stream()
+                        .filter(filme -> filme.getGeneros().contains(Genero.HORROR))
+                        .sorted(Comparator.comparing(filme -> filme.getRank()))
+                        .limit(20)
+                        .collect(Collectors.toList());
+        System.out.println("Top 20: ");
+
+        try {
+            File top20Horror = new File("top20Horror.txt");
+            top20Horror.setWritable(true);
+            FileOutputStream out = new FileOutputStream(top20Horror);
+            Writer osw = new OutputStreamWriter(out);
+            BufferedWriter bw = new BufferedWriter(osw);
+           
+            top20.forEach(filme -> {
+                try {
+                    bw.write(filme.toString() + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            bw.close();
+
+        } catch (Exception e) {
+        }
+        top20.forEach(System.out::println);
+    }
+
+    private static List<Filme> CarregarFilmes(String path, Boolean skipHeader) {
         Path arquivoFilmes = Paths.get(path);
-        
+        List<Filme> filmes = new ArrayList<>();
+
         if(!Files.exists(arquivoFilmes))
-            return;
+            return null;
 
         try(Stream<String> linhas = Files.lines(arquivoFilmes))
         {
             linhas.skip(skipHeader? 1:0)
             .forEach(linha -> {
                 Filme filme = FilmeBuilder.builder(linha.split(REGEX));
-                System.out.println(filme);
+                filmes.add(filme);
             });
         } catch (IOException e) {
-        } 
+        }
+        return filmes; 
+
     }
 }
